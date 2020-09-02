83b4889f02e5920257317909f46a34a9c9c8cf35
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-02 20:47:46 +0200

    Initial Project Add

---
+++ src/main/java/springframework/silinde87petclinic/Silinde87PetClinicApplication.java
@@ -0,0 +1,13 @@
+package springframework.silinde87petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silinde87PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Silinde87PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springframework/silinde87petclinic/Silinde87PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.silinde87petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Silinde87PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
