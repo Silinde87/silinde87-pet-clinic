e50af8f0d26e917a46a0ec26e209da0bae305e83
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-07 20:29:42 +0200

    added owners and vet lists and updated controllers. Closes #15, Closes #16

--- pet-clinic-web/src/main/java/springframework/silinde87petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframework/silinde87petclinic/bootstrap/DataLoader.java
@@ -28,14 +28,15 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner1);
 
-        System.out.println("Loaded Owners...");
-
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glenanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
+
+        System.out.println("Loaded Owners...");
+
 
         Vet vet1 = new Vet();
         vet1.setId(1L);
