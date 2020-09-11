7e4513500fd965a94f3645a07b671aff69fca14f
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-11 11:11:39 +0200

    added specialities to vets on startup. Closes #34

--- pet-clinic-data/src/main/java/springframework/silinde87petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/services/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package springframework.silinde87petclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import springframework.silinde87petclinic.model.Speciality;
 import springframework.silinde87petclinic.model.Vet;
+import springframework.silinde87petclinic.services.SpecialityService;
 import springframework.silinde87petclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    savedSpeciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
