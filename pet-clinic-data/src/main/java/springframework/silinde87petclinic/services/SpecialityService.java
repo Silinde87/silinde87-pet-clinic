a67230038781b306692477554c917cae206796c4
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-11 10:34:12 +0200

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package springframework.silinde87petclinic.services;
+
+import springframework.silinde87petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
