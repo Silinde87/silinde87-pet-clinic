6937ef6e34d24bc77888dcb56324a0e4e2938ae9
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-11 10:02:27 +0200

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package springframework.silinde87petclinic.services;
+
+import springframework.silinde87petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
