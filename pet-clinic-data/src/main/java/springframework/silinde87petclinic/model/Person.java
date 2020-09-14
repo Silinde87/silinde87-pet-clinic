bee01fb4a720f8662dd91dd224ba952736e74cd2
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-14 16:45:19 +0200

    updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/springframework/silinde87petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/model/Person.java
@@ -1,13 +1,18 @@
 package springframework.silinde87petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getFirstName() { return firstName; }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
--- pet-clinic-data/src/main/java/springframework/silinde87petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package springframework.silinde87petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
