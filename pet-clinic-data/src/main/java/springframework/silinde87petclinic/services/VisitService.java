31716d4f1c1b11fa093e161be4912175f52056b1
Silinde87 <paurodriguezmolina@gmail.com>
2020-09-15 14:24:30 +0200

    Created and implemented VisitServiceMap and VisitSDJpaService. Also added some Visit data at bootstrap. Closes #46. Closes #47.

---
+++ pet-clinic-data/src/main/java/springframework/silinde87petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package springframework.silinde87petclinic.services;
+
+import springframework.silinde87petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- pet-clinic-web/src/main/java/springframework/silinde87petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframework/silinde87petclinic/bootstrap/DataLoader.java
@@ -3,10 +3,7 @@ package springframework.silinde87petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import springframework.silinde87petclinic.model.*;
-import springframework.silinde87petclinic.services.OwnerService;
-import springframework.silinde87petclinic.services.PetTypeService;
-import springframework.silinde87petclinic.services.SpecialityService;
-import springframework.silinde87petclinic.services.VetService;
+import springframework.silinde87petclinic.services.*;
 
 import java.time.LocalDate;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +89,12 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Loaded Owners...");
 
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
